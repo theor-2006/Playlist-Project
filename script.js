a20ee0e042cde84852dcf4084ed6317b8920040f
Theo Rosenberg <124737786+theor-2006@users.noreply.github.com>
2024-02-29 23:29:16 +0000

    Code Nation updates 2/29/24. Fixed play link button not working for added songs. Still have to add 2 or 3 more songs to the base playlist then will be done.

--- script.js
+++ script.js
@@ -1,39 +1,88 @@
-// Declare variables below
+// Input variables
+let songName = document.querySelector(".song-name");
+let artists = document.querySelector(".artist");
+let songLink = document.querySelector(".song-link");
+
+let add = document.querySelector(".add");
+let plusButton = document.querySelector(".plusSym");
+
+// task 6: declare variables for your display divs: the image url, song name, artist, and song link. Go back to the HTML to check that you are using the correct class names.
+let display = document.querySelector(".display");
+let displaySong = document.querySelector(".display-song");
+let displayArtist = document.querySelector(".display-artist");
+let displayImage = document.querySelector(".display-image");
+let displayLink = document.querySelector(".display-link");
+let displayadd=document.querySelector(".form");
 
 
-// Use the body of this function to display your playlist.
+// task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
+let image = [
+"https://media.pitchfork.com/photos/5cd0900c5a46c21b62097a79/16:9/w_3632,h_2043,c_limit/Tyler-the-Creator-IGOR.jpeg"]
+let songNames = [ "I THINK"]
+let artist = ["Tyler, The Creator"]
+let songLinks = [ "https://www.youtube.com/watch?v=rkRdgFvuiYk"]
+
+
+function addSongInfo() {
+  let ImageEntry =document.querySelector(".image").value;
+  image.push(ImageEntry);
+
+  let songEntry =document.querySelector(".song-name").value;
+  songNames.push(songEntry);
+
+  let artistEntry =document.querySelector(".artist").value;
+  artist.push(artistEntry);
+
+  let linkEntry = document.querySelector(".song-link").value;
+  songLinks.push(linkEntry);
+}
+
+/******** this function empties the display divs each time the button is clicked so that your playlist does not repeatedly add the data too many times. Where should this function be placed???********/
+function emptyDisplay() {
+  displayImage.innerHTML = "";
+  displaySong.innerHTML = "";
+  displayArtist.innerHTML = "";
+  displayLink.innerHTML = "";
+}
+
 function displaySongInfo() {
 
+  emptyDisplay()
+// task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
+for(let imageNum=0; imageNum<image.length; imageNum++) {
+  displayImage.insertAdjacentHTML("beforeend",`<img src="${image[imageNum]}">`);
+}
 
-
+  for(let songs=0; songs<songNames.length; songs++){
+    displaySong.insertAdjacentHTML("beforeend","<p>"+songNames[songs]+"</p>");
   }
-  
 
-
-
-  // This function empties the divs each time the button is clicked.
-  // This prevents the playlist from repeatedly adding data.
-  // Try commenting out this function call to see what happens without it!
-  function emptySongInfo() {
-    $(".songs").empty();
-    $(".images").empty();
-    $(".artists").empty();
-    $(".lengths").empty();
-    $(".links").empty();
+  for(let art=0; art<artist.length; art++){
+    displayArtist.insertAdjacentHTML("beforeend","<p>"+artist [art]+"</p>");
   }
-  
 
-// Use the body of this function to add new songs to your playlist.
-  function addSongInfo() {
+  for(let link=0; link<songLinks.length; link++){
+    displayLink.insertAdjacentHTML("beforeend", "<a href="+songLinks[link]+">"+"play"+"</a>");
+
+  };
+}
 
 
-  }
-  
-  
-  document.querySelector("#add").addEventListener("click", function () {
-    emptySongInfo();
-    addSongInfo();
-    displaySongInfo();
-  });
-  
+
+// Event to add and display songs
+add.onclick = function() {
+  addSongInfo();
   displaySongInfo();
+};
+
+//this is for the plus button that makes the form appear and disappear when clicked 
+plusButton.addEventListener("click",function(){
+
+  displayadd.style.display="block"
+});
+plusButton.addEventListener("dblclick",function(){
+
+  displayadd.style.display="none"
+});
+
+displaySongInfo();
